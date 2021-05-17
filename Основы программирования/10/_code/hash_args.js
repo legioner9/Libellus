3c4ef735ff9cca906d12ba061ef4fea288aa7a68
Legioner9 <legioner9@inbox.ru>
2021-05-17 22:47:47 +0700

    <> edit_1 10/_code/hash_args.js

---
+++ Основы программирования/10/_code/hash_args.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+const f = (args = {}) => {
+  args.a = args.a || 'is?';
+  args.b = args.b || {};
+
+  debugger
+  console.dir(args);
+};
+
+f({ a: 'yes' });
+//args:
+// a: "yes"
+// b: {}
+
+f({ b: { c: 'c' } });
+//args:
+// a: "is?"
+// b: {c: "c"}
