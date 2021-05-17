2500bfd0905e66e3f4eec2b55cf34cde7bdaa2d4
Legioner9 <legioner9@inbox.ru>
2021-05-17 21:25:49 +0700

    <> 10/_code/introspection.js

---
+++ Основы программирования/10/_code/down_absraction.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const power = Math.pow();
+
+const square = x => power(x, 2);
+const cube = x => power(x, 3);
---
+++ Основы программирования/10/_code/introspection.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const a = function() {
+  console.log(1);
+};
+
+debugger
