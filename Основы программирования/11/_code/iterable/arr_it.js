56790eafbd6e5c943a6ddda3b743cb3ba39134c6
Legioner9 <legioner9@inbox.ru>
2021-05-27 21:18:23 +0700

    <> init 11/_code/iterable/arr_inner_this.js

---
+++ Основы программирования/11/_code/iterable/arr_it.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const it = function(arg = { step: 2 }) {
+  let index = 0;
+  return {
+    next: () => {
+      const res = {
+        done: index >= this.length,
+        value: this[index],
+      };
+      index += arg.step;
+      return res;
+    }
+  };
+
+};
+
+debugger
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// it.bind(arr);
+arr[Symbol.iterator] = it;
+
+for (const item of arr) {
+  console.log(item);
+}
