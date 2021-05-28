4173729e73d8c2e0d2700e49697a49c4fd7e1cc4
Legioner9 <legioner9@inbox.ru>
2021-05-28 14:29:56 +0700

    <> init 11/_code/itisArray/arr_reduce.js

---
+++ Основы программирования/11/_code/itisArray/arr_reduce.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const log = (a, b) => {
+  console.log(`index = ${b} \t value = ${a}`);
+};
+
+const arr = [4, 5, 8, 1];
+
+arr.map(log);
+debugger
--- Основы программирования/11/_code/itisArray/array.js
+++ Основы программирования/11/_code/itisArray/array.js
@@ -1,13 +1,21 @@
 'use strict';
-debugger
+
 const arr = [1, 2, 3];
 
-// add to tail return index
+// add to tail return length
 const s = arr.push(44);// s: 4 arr: (4) [1, 2, 3, 44]
 // remove from tail return value
 const s1 = arr.pop();// s1: 44 arr: (3) [1, 2, 3]
 
 // remove from head return value
 const s2 = arr.shift();// s2: 1 arr: (2) [2, 3]
-// add to head
+// add to head return new length
 const s3 = arr.unshift(33);// s3: 3 arr: (3) [33, 2, 3]
+
+arr[-5] = {};//arr: (3) [33, 2, 3, -5: {…}]
+
+arr.forEach((item, index, init_arr) => {
+  console.log(`${item} \t ${index} \t ${init_arr}`);
+});
+
+debugger
