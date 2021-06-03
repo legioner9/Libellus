78460f178ba9e1e1ef37c7da90ed7c5adcd7be12
Legioner9 <legioner9@inbox.ru>
2021-06-03 15:46:27 +0700

    <> init 11/_code/classes/pravet_fields/sintax.js

---
+++ Основы программирования/11/_code/classes/pravet_fields/sintax.js
@@ -0,0 +1,16 @@
+class Rectangle {
+  #height = 0;
+  #width;
+
+  constructor(height, width) {
+    this.#height = height;
+    this.#width = width;
+  }
+  static #move(){
+    debugger
+  }
+}
+
+const r = new Rectangle(4, 6);
+debugger
+
--- Основы программирования/11/_code/prototype/simple_meth_proto.js
+++ Основы программирования/11/_code/prototype/simple_meth_proto.js
@@ -3,15 +3,23 @@
 function Point(x, y) {
   this.x = x;
   this.y = y;
-  this.move = function(dx, dy) {
+  this.move_obj = function(dx, dy) {
     this.x += dx;
     this.y += dy;
   };
 }
 
+Point.prototype.move_proto = function(dx,dy){
+  this.x += dx;
+  this.y += dy;
+}
+
 const p = new Point(1, 4);
 //p: Point
-// move: ƒ (dx, dy)
+// move_obj: ƒ (dx, dy)
 // x: 1
 // y: 4
+// __proto__:
+// move_proto: ƒ (dx,dy)
+// constructor: ƒ Point(x, y)
 debugger
