2278c64611453548992c278d6023b0eb2e11495e
Legioner9 <legioner9@inbox.ru>
2021-06-04 23:00:15 +0700

    <> init _code/classes/__theory/MDN/object_create.js

---
+++ Основы программирования/11/_code/classes/__theory/MDN/object_create.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const ob = {
+  a: 1,
+  what_a() {
+    console.log(`a = ${this.a} from ob`);
+  }
+};
+
+const child_ob = Object.create(ob, { b: { value: '' } });
+//child_ob:
+// b: ""
+// __proto__:
+// a: 1
+// what_a: ƒ what_a()
+// __proto__: Object
+console.log(child_ob.a);// 1
+
+function getPropertyProto(obj, prop) {
+  if (obj.hasOwnProperty(prop))
+    return obj[prop];
+
+  else if (obj.__proto__ !== null)
+    return getPropertyProto(obj.__proto__, prop);
+
+  else
+    return undefined;
+}
+
+getPropertyProto(child_ob, 'a');
+// 1
+getPropertyProto(child_ob, 'what_a');
+// ƒ what_a() {
+//     console.log(`a = ${this.a} from ob`);
+//   }
+
+debugger
---
+++ Основы программирования/11/_code/classes/__theory/obj_vs_func_vs_class.js
@@ -0,0 +1,21 @@
+'use strict';
+debugger
+const ob = {};
+
+Object.setPrototypeOf(ob, {});
+ob.prototype = () => {};
+// const new_ob = new ob;
+
+const fn = function name_fn() {
+
+};
+
+const new_fn = new fn();
+
+const Cl = class name_Cl {
+
+};
+
+const cl = new Cl();
+
+debugger
