d6aa8faeec90b9ffbe08054d9d2eeb9356b3bfcc
Legioner9 <legioner9@inbox.ru>
2021-06-29 12:43:37 +0700

    <> init 17/_code/match.js

---
+++ Основы программирования/17/_code/match.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const m1 = 'e abc?'.match(/abc/);
+// m1: Array(1)
+//  0: "abc"
+//  groups: undefined
+//  index: 2
+//  input: "e abc?"
+
+debugger
---
+++ Основы программирования/17/_code/reg_exp_struct.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const re1 = new RegExp('abc', 'gi');
+
+//re1: /abc/gi
+// dotAll: false
+// flags: "gi"
+// global: true
+// hasIndices: false
+// ignoreCase: true
+// lastIndex: 0
+// multiline: false
+// source: "abc"
+// sticky: false
+// unicode: false
+// __proto__:
+//  compile: ƒ compile()
+//  constructor: ƒ RegExp()
+//    $1: ""
+//    $2: ""
+//    $3: ""
+//    $4: ""
+//    $5: ""
+//    $6: ""
+//    $7: ""
+//    $8: ""
+//    $9: ""
+//    $&: "F:\"
+//    $': "Node_projects\Libellus_03052k21\Основы программирования\17\_code\reg_exp.js"
+//    $+: ""
+//    $_: "F:\Node_projects\Libellus_03052k21\Основы программирования\17\_code\reg_exp.js"
+//    $`: ""
+//    arguments: (...)
+//    caller: (...)
+//    input: (...)
+//    lastMatch: (...)
+//    lastParen: (...)
+//    leftContext: (...)
+//    length: 2
+//    name: "RegExp"
+//    prototype: {constructor: ƒ, exec: ƒ, …}
+//    rightContext: (...)
+//    Symbol(Symbol.species): (...)
+//    get $1: ƒ $1()
+//    set $1: ƒ $1()
+//    get $2: ƒ $2()
+//    set $2: ƒ $2()
+//    get $3: ƒ $3()
+//    set $3: ƒ $3()
+//    get $4: ƒ $4()
+//    set $4: ƒ $4()
+//    get $5: ƒ $5()
+//    set $5: ƒ $5()
+//    get $6: ƒ $6()
+//    set $6: ƒ $6()
+//    get $7: ƒ $7()
+//    set $7: ƒ $7()
+//    get $8: ƒ $8()
+//    set $8: ƒ $8()
+//    get $9: ƒ $9()
+//    set $9: ƒ $9()
+//    get $&: ƒ $&()
+//    set $&: ƒ $&()
+//    get $': ƒ $'()
+//    set $': ƒ $'()
+//    get $+: ƒ $+()
+//    set $+: ƒ $+()
+//    get $_: ƒ $_()
+//    set $_: ƒ $_()
+//    get $`: ƒ $`()
+//    set $`: ƒ $`()
+//    get input: ƒ input()
+//    set input: ƒ input()
+//    get lastMatch: ƒ lastMatch()
+//    set lastMatch: ƒ lastMatch()
+//    get lastParen: ƒ lastParen()
+//    set lastParen: ƒ lastParen()
+//    get leftContext: ƒ leftContext()
+//    set leftContext: ƒ leftContext()
+//    get rightContext: ƒ rightContext()
+//    set rightContext: ƒ rightContext()
+//    get Symbol(Symbol.species): ƒ [Symbol.species]()
+//  dotAll: (...)
+//  exec: ƒ exec()
+//  flags: "gi"
+//  global: true
+//  hasIndices: false
+//  ignoreCase: true
+//  multiline: false
+//  source: "abc"
+//  sticky: false
+//  test: ƒ test()
+//  toString: ƒ toString()
+//  unicode: (...)
+//  Symbol(Symbol.match): ƒ [Symbol.match]()
+//  Symbol(Symbol.matchAll): ƒ [Symbol.matchAll]()
+//  Symbol(Symbol.replace): ƒ [Symbol.replace]()
+//  Symbol(Symbol.search): ƒ [Symbol.search]()
+//  Symbol(Symbol.split): ƒ [Symbol.split]()
+//  get dotAll: ƒ dotAll()
+//  get flags: ƒ flags()
+//  get global: ƒ global()
+//  get hasIndices: ƒ hasIndices()
+//  get ignoreCase: ƒ ignoreCase()
+//  get multiline: ƒ multiline()
+//  get source: ƒ source()
+//  get sticky: ƒ sticky()
+//  get unicode: ƒ unicode()
+
+const re2 = /abc/gi;
+
+
+debugger
