827eeb4b3b71a145d4f41c4a7aa572984e66c403
Legioner9 <legioner9@inbox.ru>
2021-07-05 18:55:01 +0700

    <> edit 1_folium_.js

--- @Essentia_hoc/Collectio_Function_design_templates/Constructione_Principium/Secundo_gradu/Truncus_wraper/Ramus_timers/1_folium_.js
+++ @Essentia_hoc/Collectio_Function_design_templates/Constructione_Principium/Secundo_gradu/Truncus_wraper/Ramus_timers/1_folium_.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const wrapKillTimteFlag = (delay, fn) => {
+  // define flag
+  let flag = false;
+  const timeout = setTimeout(function() {
+    console.dir(this);
+    flag = true;
+  }, delay, 'args');
+  // timeout.close();
+  debugger
+  return (...args) => {
+    console.dir({ flag, args });
+  };
+};
+
+wrapLifeTimteFlag(1000, () => {
+})('e');
