d58285d673ae9dbb0390ee717fd2934cb66743fe
Legioner9 <legioner9@inbox.ru>
2021-07-05 13:08:10 +0700

    <> paratus elOnce.js

---
+++ @Essentia_hoc/Collectio_Function_design_templates/Constructione_Principium/Primo_gradu/elOnce.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const once = (fn) => {
+  return (...args) => {
+    if (!fn) return;
+    const res = fn(...args);
+    fn = null;
+    return res;
+  };
+};
+
+const fn = (...args) => console.dir(...args);
+
+const onceFn = once(fn);
+
+onceFn('unus');
+onceFn('duo');
+
