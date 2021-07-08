da7a50638b7e207c41983ceae82505839850a2c2
Legioner9 <legioner9@inbox.ru>
2021-07-08 22:59:09 +0700

    <> paratus 2_folium_mutationClosure.js

--- @Essentia_hoc/Collectio_Function_design_templates/Constructione_Principium/Secundo_gradu/Ramus_mixToSelf/2_folium_mutationClosure.js
+++ @Essentia_hoc/Collectio_Function_design_templates/Constructione_Principium/Secundo_gradu/Ramus_mixToSelf/2_folium_mutationClosure.js
@@ -3,24 +3,26 @@
 debugger
 const limit = (max, fn) => {
   let counter = 0;
-  const closureObj = { counter, max };
+  // const closureObj = { counter, max };
 
-  return (...args) => {
-    fn.args = args;
-    fn.closureObj = closureObj;
+  const wrapper = (...args) => {
+    wrapper.args = args;
+    wrapper.closureObj = counter;
 
-    counter++;
-
-    console.dir({ fn, args, closureObj });
-    if (counter < max) return fn(...args);
+    console.dir({ fn, args, counter });
+    if (counter < max) {
+      counter++;
+      return fn(...args);
+    }
   };
+  return wrapper;
 };
 
 const fn = (...args) => ({ args });
+
 const wrapFn = limit(1, fn);
 
 // test area:
-wrapFn('primus');
 
 console.log(`test first call: ${wrapFn('primus')} , with closure: ${wrapFn.closureObj}`);
 console.log(`test second call: ${wrapFn('secundus')} , with closure: ${wrapFn.closureObj}`);
--- @Essentia_hoc/Collectio_Function_design_templates/Constructione_Principium/Secundo_gradu/generativusFucus.js
+++ @Essentia_hoc/Collectio_Function_design_templates/Constructione_Principium/Secundo_gradu/generativusFucus.js
@@ -1,31 +1,35 @@
 'use strict';
 
 const generativusFucus = (fn) => {
-  debugger
 
   const closureForFn = 'closure';
   const thisForFn = { this: 'this' };
   const fucus = function(...args) {
 
-    fn.args = args;
-    fn.closureForFn = closureForFn;
-    fn.thisForFn = thisForFn;
-    fn.bind(thisForFn);
+    const bfn = fn.bind(thisForFn);
+    fucus.args = args;
+    fucus.closureForFn = closureForFn;
+    fucus.thisForFn = thisForFn;
 
     fucus.printFn = (fn) => {
       console.log(fn);
       return fucus;
     };
 
-    return fn(...args);
+    return bfn(...args);
   };
   return fucus;
 };
 
 const fn = function(...args) {
-  debugger
+  console.dir({ args, this: this });
 };
 
+// Use
 const fucus = generativusFucus(fn);
 fucus('a');
+
+// fucus (as a functional object) after call has the following properties:
+const { args, closureForFn, thisForFn } = fucus;
+console.dir({ args, closureForFn, thisForFn });
 debugger
