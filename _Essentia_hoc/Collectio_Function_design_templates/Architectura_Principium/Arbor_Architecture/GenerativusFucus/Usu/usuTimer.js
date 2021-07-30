5e044cd4f576a16ecd0f3fa74303fd99810d259a
Legioner9 <legioner9@inbox.ru>
2021-07-31 01:07:53 +0700

    <> paratus usuTimer.js

---
+++ _Essentia_hoc/Collectio_Function_design_templates/Architectura_Principium/Arbor_Architecture/GenerativusFucus/Usu/usuTimer.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { timer } = require('artus/fascisOperati/timer');
+
+const fn = function(...args) {
+  console.dir({ args, this: this });
+};
+
+const fucus = timer(fn).limit(2).timer(1200);
+fucus(0);
+setTimeout(() => {
+  fucus('primus');
+}, 100);
+
+setTimeout(() => {
+  fucus('secundus');
+}, 500);
+
