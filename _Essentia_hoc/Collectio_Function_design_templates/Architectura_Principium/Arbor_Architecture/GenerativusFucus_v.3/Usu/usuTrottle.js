34517003c38e27a0cf8044d86c8d7c4a9163150a
Legioner9 <legioner9@inbox.ru>
2021-08-05 15:47:12 +0700

    <> artus.v.2 #1

--- _Essentia_hoc/Collectio_Function_design_templates/Architectura_Principium/Arbor_Architecture/GenerativusFucus_v.3/Usu/usuTrottle.js
+++ _Essentia_hoc/Collectio_Function_design_templates/Architectura_Principium/Arbor_Architecture/GenerativusFucus_v.3/Usu/usuTrottle.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { memoizeAsync } = require('artus/fascisOperati/memoizeAsync');
+const { trottle } = require('artus.v.2/fascisOperati/trottle');
 
 const fn = function(...args) {
   console.dir({ args, this: this });
