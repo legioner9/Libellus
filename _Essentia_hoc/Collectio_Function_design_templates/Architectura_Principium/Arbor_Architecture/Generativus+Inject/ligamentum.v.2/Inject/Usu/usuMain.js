003d9e51b7f54eedb7de249b41c40d02495ea611
Legioner9 <legioner9@inbox.ru>
2021-08-07 08:52:33 +0700

    <> ligamentum.v.2 #2

--- _Essentia_hoc/Collectio_Function_design_templates/Architectura_Principium/Arbor_Architecture/Generativus+Inject/ligamentum.v.2/Inject/Usu/usuMain.js
+++ _Essentia_hoc/Collectio_Function_design_templates/Architectura_Principium/Arbor_Architecture/Generativus+Inject/ligamentum.v.2/Inject/Usu/usuMain.js
@@ -1,7 +1,30 @@
 'use strict';
 
-const { memoizeAsync } = require('artus/fascisOperati/memoizeAsync');
+const memoizeAsync = require('artus.v.2/fascisOperati/memoizeAsync');
+debugger
+memoizeAsync.lengthCache(3);
 
-const fn = function(...args) {
-  console.dir({ args, this: this });
+const fn = (x, callback) => {
+  if (typeof x !== 'number') callback('argument is not a number');
+  callback(null, x);
 };
+const callback = (...args) => args;
+
+const mfn = memoizeAsync(fn);
+
+mfn.on('add', (...data) => {
+  console.log('event add called with: ', data);
+});
+// mfn.addPrototypes(objProto);
+
+mfn(1, callback);
+debugger
+mfn.clear();
+debugger
+mfn(1, callback);
+mfn(1, callback);
+debugger
+mfn(2, callback);
+mfn(3, callback);
+mfn(4, callback);
+debugger
