9ffa70876dde14cb86f311a0257c46ef4685fb1d
Legioner9 <legioner9@inbox.ru>
2021-08-08 17:07:46 +0700

    <> far communis.methods close

--- _Essentia_hoc/Collectio_Function_design_templates/Architectura_Principium/Arbor_Architecture/Generativus+Inject/ligamentum.v.2/Generativus/Usu/g_operati.js
+++ _Essentia_hoc/Collectio_Function_design_templates/Architectura_Principium/Arbor_Architecture/Generativus+Inject/ligamentum.v.2/Generativus/Usu/g_operati.js
@@ -5,7 +5,7 @@ const { timer } = require('artus.v.2/fascisOperati/g_fascis');
 const fn = function(...args) {
   console.dir({ args, this: this });
 };
-
+debugger
 const fucus = timer(fn).limit(2).timer(1200);
 fucus(0);
 setTimeout(() => {
