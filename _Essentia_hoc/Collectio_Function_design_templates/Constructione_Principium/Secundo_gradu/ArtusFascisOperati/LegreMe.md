021d01d70ca5da46b5aee8b80de56b6dec2ac552
Legioner9 <legioner9@inbox.ru>
2021-08-10 01:01:39 +0700

    <> add ArtusFascisOperati to Secundo_gradu

--- _Essentia_hoc/Collectio_Function_design_templates/Constructione_Principium/Secundo_gradu/ArtusFascisOperati/LegreMe.md
+++
@@ -1,7 +0,0 @@
-## [v.1](node_modules\v.1\test\main.js)
-
-- main flow
-
-## [v.2](node_modules\v.2\test\main.js)
-
-- using:
---
+++ _Essentia_hoc/Collectio_Function_design_templates/Constructione_Principium/Secundo_gradu/ArtusFascisOperati/Unitio/Usu/i_operati.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { memoizeAsync } = require('artus/fascisOperati/i_fascis');
+
+const fn = (x, callback) => {
+  if (typeof x !== 'number') callback('argument is not a number');
+  callback(null, x);
+};
+const callback = (...args) => args;
+
+const add = (...data) => {
+  console.log('event add called with: ', data);
+};
+
+const mfn = memoizeAsync(fn).setLengthCache(3).onEventCache('add', add);
+
+mfn(1, callback);
+debugger
+mfn.clearCache();
+debugger
+mfn(1, callback);
+mfn(1, callback);
+debugger
+mfn(2, callback);
+mfn(3, callback);
+mfn(4, callback);
+debugger
