f77f7d46d921e26423dff3cbc9f686c4b3915a22
Legioner9 <legioner9@inbox.ru>
2021-08-10 13:09:33 +0700

    <> add todo

--- _Essentia_hoc/Collectio_Function_design_templates/Architectura_Principium/Arbor_Architecture/ArtusFascisOperati/Unitio.v.2/Usu/tubus_i_g.js
+++ _Essentia_hoc/Collectio_Function_design_templates/Architectura_Principium/Arbor_Architecture/ArtusFascisOperati/Unitio.v.2/Usu/tubus_i_g.js
@@ -7,16 +7,16 @@ const fn = (x, callback) => {
   if (typeof x !== 'number') callback('argument is not a number');
   else callback(null, x);
 };
-const callback = (...args) => console.log (args);
+const callback = (...args) => console.log(args);
 const add = (...data) => {
   console.log('event add called with: ', data);
 };
 debugger
 
-const t_fucus = timer(fn)//.setLimit(2).setTimer(1200);
-const a_t_fucus = memoizeAsync(t_fucus)//.setLengthCache(3).onEventCache('add', add);
+const t_fucus = timer(fn);//.setLimit(2).setTimer(1200);
+const a_t_fucus = memoizeAsync(t_fucus);//.setLengthCache(3).onEventCache('add', add);
 debugger
-
+// TODO: error composition
 a_t_fucus(0, callback);
 setTimeout(() => {
   a_t_fucus(1, callback);
