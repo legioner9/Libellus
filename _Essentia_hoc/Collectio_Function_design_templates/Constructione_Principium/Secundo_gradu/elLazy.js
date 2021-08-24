f6f08122174e45f82c341e25eec670450d06daeb
[win]Legioner9 <legioner9@inbox.ru>
2021-08-24 15:35:34 +0700

    <> paratus lazy

--- _Essentia_hoc/Collectio_Function_design_templates/Constructione_Principium/Secundo_gradu/elLazy.js
+++ _Essentia_hoc/Collectio_Function_design_templates/Constructione_Principium/Secundo_gradu/elLazy.js
@@ -1,1 +1,22 @@
-'use '
+'use strict';
+
+const scaleble = (image) => {
+  image.scale = () => console.log(image);
+};
+
+const lazyed = (image) => {
+  const scale = image.scale;
+  image.scale = () => setImmediate(() => scale());
+};
+
+const img = { a: 'a' };
+
+scaleble(img);
+console.log('start');
+img.scale();
+console.log('end');
+
+lazyed(img);
+console.log('start');
+img.scale();
+console.log('end');
